7124a5b1bcb0284e671dd6679ea694a83fa40a1d
BLOT Martial <martial.blot@outlook.fr>
2019-02-26 10:42:19 +0100

    Exercies JS

---
+++ Downloads/movie/movie.js
@@ -0,0 +1,6 @@
+const movieName = "Pulp Fiction";
+const director = "Quentin Tarantino";
+const year = 1994;
+const message = movieName + " réalisé par " + director + " en " + year + " .";
+
+alert(message);
